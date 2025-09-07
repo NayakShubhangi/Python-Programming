3902d2375425a41bf6f21233ecae0ce7e8ddb482
NayakShubhangi <shubhangin2012@gmail.com>
2025-09-07 12:36:22 -0700

    Updates and creation

---
+++ Python_Code/Python_Input.py
@@ -0,0 +1,12 @@
+# TODO TASK TWO:
+# Get (argv / command line) input from the user (refer to Command_Line_Arguments.py)
+# and display it in a function
+# Create a new workflow (Run_python_with_Inputs.yml) and take string input,
+# then pass it into the python call
+import sys
+
+def Display_Input(user_input: str):
+    print(user_input)
+
+user_input = sys.argv[1]
+Display_Input(user_input)
